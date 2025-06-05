0dd86471716b3768a68cb0bccb49f27bcde19db3
FRANK <frankmascco@gmail.com>
2025-06-05 05:53:30 -0500

    feat: integrate Swagger for API documentation

--- api-gateway/src/main/java/com/programming/frank/api_gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/programming/frank/api_gateway/routes/Routes.java
@@ -10,6 +10,8 @@ import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+
 @Configuration
 public class Routes {
 
@@ -21,6 +23,14 @@ public class Routes {
     }
 
     @Bean
+    public RouterFunction<ServerResponse> productServiceSwaggerRoute(){
+        return GatewayRouterFunctions.route("product_service_swagger")
+                .route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8081"))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+
+    @Bean
     public RouterFunction<ServerResponse> clientServiceRoute(){
         return GatewayRouterFunctions.route("client_service")
                 .route(RequestPredicates.path("/api/client/**"), HandlerFunctions.http("http://localhost:8082"))
@@ -28,9 +38,25 @@ public class Routes {
     }
 
     @Bean
+    public RouterFunction<ServerResponse> clientServiceSwaggerRoute(){
+        return GatewayRouterFunctions.route("client_service_swagger")
+                .route(RequestPredicates.path("/aggregate/client-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8082"))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+
+    @Bean
     public RouterFunction<ServerResponse> quotationServiceRoute(){
         return GatewayRouterFunctions.route("quotation_service")
                 .route(RequestPredicates.path("/api/quotation/**"), HandlerFunctions.http("http://localhost:8083"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> quotationServiceSwaggerRoute(){
+        return GatewayRouterFunctions.route("quotation_service_swagger")
+                .route(RequestPredicates.path("/aggregate/quotation-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8083"))
+                .filter(setPath("/api-docs"))
                 .build();
     }
 
---
+++ client-microservice/src/main/java/com/programming/frank/client_microservice/config/OpenAPIConfig.java
@@ -0,0 +1,24 @@
+package com.programming.frank.client_microservice.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI clientServiceAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Client Service API")
+                        .description("This is the REST API for Client Service")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("You can refer to the Client Service Wiki Documentation")
+                );
+    }
+}
