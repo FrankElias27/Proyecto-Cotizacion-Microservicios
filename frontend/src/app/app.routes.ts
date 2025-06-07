95670fcf88c8c67da21b8a732471be6d9d9e2ddb
FRANK <frankmascco@gmail.com>
2025-06-07 07:01:59 -0500

    feat: complete sidebar implementation with navigation and styling

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { DashboardComponent } from './pages/user/dashboard/dashboard.component';
 import { WelcomeComponent } from './pages/user/welcome/welcome.component';
+import { ClientComponent } from './pages/user/client/client.component';
+import { ProductComponent } from './pages/user/product/product.component';
+import { QuotationComponent } from './pages/user/quotation/quotation.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -10,6 +13,9 @@ export const routes: Routes = [
     component: DashboardComponent,
     children: [
       { path: 'welcome', component: WelcomeComponent },
+      { path: 'client', component: ClientComponent },
+      { path: 'product', component: ProductComponent },
+      { path: 'quotation', component: QuotationComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
     ]
   }
