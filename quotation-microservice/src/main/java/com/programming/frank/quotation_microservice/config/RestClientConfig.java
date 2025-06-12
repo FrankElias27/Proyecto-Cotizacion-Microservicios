9716a2b49c8c51fcee059d4ce221e075416bcd09
FRANK <frankmascco@gmail.com>
2025-06-12 08:50:23 -0500

    feat: add Grafana observability stack (Grafana, Prometheus, Tempo)

--- quotation-microservice/src/main/java/com/programming/frank/quotation_microservice/config/RestClientConfig.java
+++ quotation-microservice/src/main/java/com/programming/frank/quotation_microservice/config/RestClientConfig.java
@@ -1,6 +1,8 @@
 package com.programming.frank.quotation_microservice.config;
 
 import com.programming.frank.quotation_microservice.client.ClientClient;
+import io.micrometer.observation.ObservationRegistry;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,16 +11,20 @@ import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
+@RequiredArgsConstructor
 public class RestClientConfig {
 
     @Value("${client.service.url}")
     private String clientServiceUrl;
+
+    private final ObservationRegistry observationRegistry;
 
     @Bean
     public ClientClient clientClient(){
 
         RestClient restClient = RestClient.builder()
                 .baseUrl(clientServiceUrl)
+                .observationRegistry(observationRegistry)
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
--- quotation-microservice/src/main/java/com/programming/frank/quotation_microservice/config/RestProductConfig.java
+++ quotation-microservice/src/main/java/com/programming/frank/quotation_microservice/config/RestProductConfig.java
@@ -2,6 +2,9 @@ package com.programming.frank.quotation_microservice.config;
 
 import com.programming.frank.quotation_microservice.client.ClientClient;
 import com.programming.frank.quotation_microservice.client.ProductClient;
+import io.micrometer.observation.ObservationRegistry;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,16 +13,19 @@ import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
+@RequiredArgsConstructor
 public class RestProductConfig {
 
     @Value("${product.service.url}")
     private String productServiceUrl;
+    private final ObservationRegistry observationRegistry;
 
     @Bean
     public ProductClient productClient(){
 
         RestClient restClient = RestClient.builder()
                 .baseUrl(productServiceUrl)
+                .observationRegistry(observationRegistry)
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
